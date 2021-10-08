9a70dd50c417cf86b0c7469442db39c3c384b70b
Nuria Braiza <nuriabraiza@gmail.com>
2021-10-08 11:11:07 -0300

    final commit

--- js/contacto.js
+++ js/contacto.js
@@ -1,40 +1,28 @@
 const submitir = document.querySelector(".submit");
 
-submitir.addEventListener("click", enviado);
-function enviado(evento) {
-  var nombre = document.contacto.nombre.value;
-  var apellido = document.contacto.apellido.value;
-  var mail = document.contacto.mail.value;
-  var consulta = document.contacto.consulta.value;
-  if (nombre == "" && apellido == "" && mail == "" && consulta == "") {
-    $("main").prepend(`
-      <div class="alert alert-warning d-flex align-items-center" id="warning" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Warning:"><use xlink:href="#exclamation-triangle-fill"/></svg>
-    <div>
-      Por favor completar el formulario
-    </div>
-  </div>`);
-    const alert = document.getElementClassName("alert");
-
-    $(".alert").fadeIn(1000);
-
-    setTimeout();
-    evento.preventDefault();
+function enviado() {
+  if (
+    $("#name").val() != "" &&
+    $("#apellido").val() != "" &&
+    $("#inputEmail4").val() != "" &&
+    $("#texto").val() != ""
+  ) {
+    $("#modal-contacto").modal("show");
   } else {
-    $("main").prepend(`
-    <div class="alert alert-success d-flex align-items-center" id="success" role="alert">
-    <svg class="bi flex-shrink-0 me-2" width="24" height="24" role="img" aria-label="Success:"><use xlink:href="#check-circle-fill"/></svg>
-    <div >
-      Tu consulta fue enviada con exito. Nuestro equipo estará contactandote por mail.
-    </div>
-  </div>`);
-
-    $(".alert").fadeIn(1000);
-
-    setTimeout();
+    $("#modal-incompleto").modal("show");
   }
 }
+const btnClose = document.getElementById("close");
+btnClose.addEventListener("click", () => {
+  window.location = "index.html";
+});
 
-setTimeout(function () {
-  $(".alert").fadeOut(5000);
+const btnCont = document.getElementById("continuar");
+btnCont.addEventListener("click", () => {
+  $("#modal-incompleto").modal("hide");
+});
+
+submitir.addEventListener("click", (event) => {
+  enviado();
+  event.preventDefault();
 });
